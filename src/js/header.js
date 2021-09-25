8b1b5e49df698938b27720022403f14945e35e0a
Yuri Shvetsov <shvecov@media5.com>
2021-09-25 11:17:31 +0500

    New feature
    Auto display of header

--- src/js/header.js
+++ src/js/header.js
@@ -82,4 +82,76 @@ function initMenu() {
   }
 }
 
+function initHeader() {
+  const header = document.querySelector('.js-header')
+  const firstSection = document.querySelector('.section')
+
+  let isFixed = false
+  let maxScrollTop = calcMaxScrollTop()
+  let lastScrollTop = getScrollTop()
+
+  init()
+
+  window.addEventListener('scroll', event => {
+    const curScrollTop = getScrollTop()
+
+    if (!isFixed && curScrollTop > maxScrollTop && curScrollTop < lastScrollTop) {
+      fix()
+    } else if (isFixed && curScrollTop > maxScrollTop && curScrollTop > lastScrollTop) {
+      unFix()
+    } else if (isFixed && curScrollTop < maxScrollTop) {
+      unFix()
+    }
+
+    lastScrollTop = curScrollTop
+  })
+
+  window.addEventListener('resize', event => {
+    maxScrollTop = calcMaxScrollTop()
+    lastScrollTop = getScrollTop()
+    init()
+  })
+
+  function init() {
+    if (!isFixed && lastScrollTop > maxScrollTop) {
+      fix()
+    } else if (isFixed && lastScrollTop < maxScrollTop) {
+      unFix()
+    }
+  }
+
+  function getScrollTop() {
+    return document.documentElement.scrollTop || document.body.scrollTop
+  }
+
+  function calcMaxScrollTop() {
+    const headerHeight = header.offsetHeight
+    const firstSectionHeight = firstSection.offsetHeight
+    const firstSectionTop = firstSection.offsetTop
+
+    return firstSectionTop + firstSectionHeight - headerHeight
+  }
+
+  function fix() {
+    isFixed = true
+    header.classList.add('header_fixed')
+    header.classList.add('header_fixed_visible')
+  }
+
+  function unFix() {
+    isFixed = false
+    header.classList.remove('header_fixed_visible')
+    header.classList.add('header_fixed_hidden')
+
+    header.addEventListener('animationend', removeClass)
+
+    function removeClass() {
+      header.classList.remove('header_fixed_hidden')
+      header.classList.remove('header_fixed')
+      header.removeEventListener('animationend', removeClass)
+    }
+  }
+}
+
+initHeader()
 initMenu()
